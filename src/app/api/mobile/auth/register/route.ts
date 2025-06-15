fb28ee9ea1297c18e4dafa185e66373906c24061
bagaskalih <bagaskalihputra.bkp@gmail.com>
2025-06-15 15:47:05 +0700

    feat: implement user registration API with validation and token generation

---
+++ src/app/api/mobile/auth/register/route.ts
@@ -0,0 +1,86 @@
+import { prisma } from "@/lib/db";
+import { encode } from "next-auth/jwt";
+import bcrypt from "bcryptjs";
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Authorization, Content-Type, X-Requested-With",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+}
+
+export async function POST(req: Request) {
+  try {
+    const requestBody = await req.json();
+    const { name, email, password } = requestBody;
+
+    if (!name || !email || !password) {
+      return new Response(
+        JSON.stringify({ error: "All fields are required" }),
+        {
+          status: 400,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return new Response(JSON.stringify({ error: "User already exists" }), {
+        status: 409,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        role: "USER",
+        password: hashedPassword,
+      },
+    });
+
+    const token = await encode({
+      secret: process.env.NEXTAUTH_SECRET!,
+      token: { id: newUser.id, role: newUser.role, password: "" },
+    });
+
+    return new Response(
+      JSON.stringify({ user: { id: newUser.id, name, email }, token }),
+      {
+        status: 201,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
